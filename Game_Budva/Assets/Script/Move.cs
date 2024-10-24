e60cf2ba5eece81dd65f837c9cc113b016d824a9
Nikolay1166 <rom.nik66@gmail.com>
2024-10-24 21:35:49 +0200

    24.10

--- Game_Budva/Assets/Script/Move.cs
+++ Game_Budva/Assets/Script/Move.cs
@@ -1,31 +1,71 @@
-//using System.Collections;
-//using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Tilemaps;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Move : MonoBehaviour
 {
     public int speed = 5; 
     public float jump = 2; 
+    private int score = 0;
+    public TMP_Text scoreText;
     public bool grounded = false;
-      private void Update(){
-       if(Input.GetKey(KeyCode.D)) 
-           transform.Translate(new Vector3(1,0,0) * Time.deltaTime * speed);
-       if(Input.GetKey(KeyCode.A)) 
-           transform.Translate(new Vector3(-1,0,0) * Time.deltaTime * speed);    
-              
-       if (Input.GetKey(KeyCode.Space) && grounded)
-            GetComponent<Rigidbody2D>().AddForce(transform.up * jump, ForceMode2D.Impulse);   
+    private AudioSource audio;
+    public AudioClip audiojump, audiotake;
+    public SpriteRenderer SpriteRenderer;
+   
+    private void Awake()
+    {
+     audio = GetComponent<AudioSource>();
+     
     }
+
+    private void Update(){
+        SpriteRenderer SpriteRenderer = gameObject.GetComponent<SpriteRenderer>();   
+       if(Input.GetKey(KeyCode.D)) {
+           transform.Translate(new Vector3(1,0,0) * Time.deltaTime * speed);
+           
+            SpriteRenderer.flipX = false;
+         }
+       if(Input.GetKey(KeyCode.A)) {
+           transform.Translate(new Vector3(-1,0,0) * Time.deltaTime * speed);    
+           SpriteRenderer.flipX = true;
+       }   
+       if (Input.GetKeyDown(KeyCode.Space) && grounded){
+            audio.clip =  audiojump;
+            audio.Play(); 
+            GetComponent<Rigidbody2D>().AddForce(transform.up * jump, ForceMode2D.Impulse); 
+       }
+    }
+
 
     private void OnCollisionEnter2D(Collision2D other){
          grounded = true; 
+          audio.clip =  audiojump;
+            audio.Play(); 
     }
-   // private void OnCollisionExit2D(Collision2D other){
-      //   grounded = false;
+    private void OnCollisionExit2D(Collision2D other){
+         grounded = false;
+    }
 
- 
-   }
+      private void OnTriggerEnter2D(Collider2D other)
+{
+    if (other.gameObject.tag == "Colect box"){
+     Destroy (other.gameObject);
+     audio.clip =  audiotake;
+        audio.Play();
+     score++;
+     if (score != 4)
+         scoreText.text = "Box: " + score; 
+     else
+         scoreText.text = "you win!";
+    }
+    
+}
 
-//}
+}
+
+
+
+
 
 
